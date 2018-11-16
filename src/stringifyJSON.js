142e354e026872da35232050c11b234e1be5750f
Mike Lopez <mikeglopez.91@gmail.com>
2018-11-15 20:17:07 -0600

    "Pseudocode stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,44 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // if null
+  //    return 'null'
+
+  // if undefined OR function (typeof input === 'function')
+  //    return undefined;
+
+  // if number
+  //    return number.toString();
+
+  // if string
+  //    return string
+
+  // if boolean
+  //    return " + boolean + "
+
+  // if array
+  // var ouput = '[';
+  //    for each element in the array..
+  //        output += stringifyJSON(element)
+  // output += "]"
+  // return output
+
+  // if object
+  //  var output = '{'
+  //    for (key in object)..
+  //      output += 'key:' + stringifyJSON(object[key]) + ','
+  // return output + '}'
+
 };
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#Description
+
+// Undefined	"undefined"
+// Null	"object" (see below)
+// Boolean	"boolean"
+// Number	"number"
+// String	"string"
+// Symbol (new in ECMAScript 2015)	"symbol"
+// Host object (provided by the JS environment)	Implementation-dependent
+// Function object (implements [[Call]] in ECMA-262 terms)	"function"
+// Any other object	"object"
